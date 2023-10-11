83122010502724df030982e50a69da6bc48d5f6b
nabilsaragih <nabilsaragih33@gmail.com>
2023-10-11 21:27:31 +0800

    PT4 DONEEE IDC BOUT HOW SIMPLE IT IS

---
+++ PT4/scripts/php/loginDataHandler.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    echo "Username: " . $username . "<br>";
+    echo "Email: " . $email . "<br>";
+    echo "Password: " . $password . "<br>";
+}
+?>
